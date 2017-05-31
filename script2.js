57983213cfa27d7034828c1b45b712d2992a29e4
Ericson <ericson@shmok.me>
2017-05-31 21:32:49 +0800

    third commit

--- script2.js
+++ script2.js
@@ -1,32 +1,25 @@
-var highlight;
-var typed;
-var points = 0;
-var seconds = 122;
-var time = document.querySelector(".time");
-var timerDiv = document.querySelector(".time");
-var button = document.querySelector("button");
-var score = document.querySelector(".score");
-var word = document.querySelector(".word");
-
+if(document.readyState === "complete") {
+// Checking the letter that is typed.
 function checkKey(t) {
   typed = String.fromCharChode(t.which);
-  for (var i = 0; i < highlight.length; i++) {
-    if (highlight[i].innerHTML === typed) {
-      if (highlight[i].classList.contains("bg")) {
+  for (var i = 0; i < spans.length; i++) {
+    if (spans[i].innerHTML === typed) {
+      if (spans[i].classList.contains("bg")) {
         continue;
-      } else if (highlight[i].classList.contains("bg") === false && highlight[i-1] === undefined
-      || highlight[i-1].classList.contains("bg") !== false) {
-        highlight[i].classList.add("bg");
-        break;
+        } else if (spans[i].classList.contains("bg") === false
+          && spans[i-1] === undefined
+          || spans[i-1].classList.contains("bg") !== false) {
+          spans[i].classList.add("bg");
+          break;
         }
       }
     }
   var checker = 0;
-  for (var j = 0; j < highlight.length; j++) {
-    if (highlight[j].className === "span bg") {
+  for (var j = 0; j < spans.length; j++) {
+    if (spans[j].className === "span bg") {
       checker++;
     }
-    if (checker === highlight.length) {
+    if (checker === spans.length) {
       points++;
       score.innerHTML = points;
     }
@@ -36,10 +29,6 @@ button.addEventListener("click", function(t) {
     countdown();
     random();
     button.disabled = true;
-  });
-button.addEventListener("click", function(t) {
-    countdown();
-    random();
-    button.disabled = true;
     document.addEventListener("keydown", checkKey, false);
   });
+}
